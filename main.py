489483a69af68837dcb402ba76f562b0787a9448
Dipankar Pal <dipankarpal5050@gmail.com>
2020-11-12 15:57:52 +0000

    :smile: more memed added

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from time import sleep
 ids = []
 
 
-url = 'https://tg.i-c-a.su/json/programmerjokes/2?limit=100'
+url = 'https://tg.i-c-a.su/json/programmerjokes/3?limit=100'
 r = urlopen(url)
 data = json.loads(str(r.read().decode("utf-8")))
 
@@ -28,16 +28,18 @@ for message in data['messages']:
     if int(id) in ids:
         print(str(id)+': already in the database')
         continue
-
-    # download image
-    img_url = 'https://tg.i-c-a.su/media/programmerjokes/'+str(id)
-    response = urlopen(img_url)
-    img_file = open('memes/'+str(id)+'.png','wb')
-    img_file.write(response.read())
-    img_file.close()
-    print(str(id)+': downloaded')
-    sleep(2)
-    ids.append(int(message['id']))
+    try:
+        # download image
+        img_url = 'https://tg.i-c-a.su/media/programmerjokes/'+str(id)
+        response = urlopen(img_url)
+        img_file = open('memes/'+str(id)+'.png','wb')
+        img_file.write(response.read())
+        img_file.close()
+        print(str(id)+': downloaded')
+        sleep(2)
+        ids.append(int(message['id']))
+    except:
+        print("Error while downloading:"+str(id))
 
 
 with open('ids.data', 'wb') as filehandle:
