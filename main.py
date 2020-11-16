bfd19ad235d762679d3b3d7a6a0637a1ac0aab00
Dipankar Pal <dipankarpal5050@gmail.com>
2020-11-16 06:28:01 +0000

    :smile: memes added

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from time import sleep
 ids = []
 
 
-url = 'https://tg.i-c-a.su/json/programmerjokes/13?limit=100'
+url = 'https://tg.i-c-a.su/json/programmerjokes/15?limit=100'
 r = urlopen(url)
 data = json.loads(str(r.read().decode("utf-8")))
 
